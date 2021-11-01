a953ac86a261d1aac805f179ba2ab77bdef956ea
sonandrew <andrewz.jackson667@gmail.com>
2021-10-31 21:10:25 -0500

    Created a function to check a cave and tells you if the dragon eats you or gives you treasure

--- dragon.py
+++ dragon.py
@@ -15,3 +15,20 @@ def chooseCave():
         cave = input()
 
     return cave
+
+# Check the cave function
+def checkCave(chosenCave):
+    print('You approach the cave...')
+    time.sleep(2)
+    print('It is dark and spooky...')
+    time.sleep(2)
+    print('A large dragon jumps out in front of you! He opens his jaws and...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+        print('Gives you his treasure!')
+    else:
+        print('Gobbles you down in one bite!')
