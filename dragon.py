50615900898e79f96a6574091b79f29ce49ddf9b
sonandrew <andrewz.jackson667@gmail.com>
2021-10-31 20:56:30 -0500

    created README.md file and added description of game

--- dragon.py
+++ dragon.py
@@ -4,3 +4,4 @@ import time
 def displayIntro():
     print('''You are in a land full of dragons. In front of you , you see two caves. In one cave, the dragon is friendly and will share his treasure with you. The other dragon is greedy and hungry, and will eat you on sight.''')
     
+displayIntro()
