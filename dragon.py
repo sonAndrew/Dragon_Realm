1e9ad10ae075214edbec4ee0257b145b9eaf8555
sonandrew <andrewz.jackson667@gmail.com>
2021-10-31 21:05:00 -0500

    created the chooseCave() function to choose which cave to go into

--- dragon.py
+++ dragon.py
@@ -1,7 +1,17 @@
 import random
 import time
 
+# Introduction to game function
 def displayIntro():
     print('''You are in a land full of dragons. In front of you , you see two caves. In one cave, the dragon is friendly and will share his treasure with you. The other dragon is greedy and hungry, and will eat you on sight.''')
     
-displayIntro()
+    print()
+
+# Choose which cave to go into function
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
+
+    return cave
